236dfca25de3689beb78e7d220e51b2fe6356ca3
Snejochekk <Snejochekk@mail.ru>
2023-10-20 13:36:55 +0300

    Вычислить выражения

--- HWork_1-2/main.c
+++ HWork_1-2/main.c
@@ -2,20 +2,21 @@
 
 int main()
 {
-    int a;
-    int c;
-    int b;
-    int d;
-    a=5,c=5;
-    printf("a=%d, c=%d, b=%d, d=%d\n", a,c,b,d);
-    //a=a+b-2;
-    //printf("a=a+b-2\n");
-    printf("a=%d, c=%d, b=%d, d=%d\n", a,c,b,d);
-    c=c+1, d=c-a+d;
-    printf("a=%d, c=%d, b=%d, d=%d\n", a,c,b,d);
-    a=a*c, c=c-1;
-    printf("a=%d, c=%d, b=%d, d=%d\n", a,c,b,d);
-    a=a/10, c=c/2, b=b-1, d=d*(c+b+a);
-    printf("a=%d, c=%d, b=%d, d=%d\n", a,c,b,d);
+    int a = 5;
+    int c = 4;
+    int b = 7;
+    int d = 2;
+    c = a;
+    printf("a = %d, c = %d, b = %d, d = %d\n", a,c,b,d);
+    a = a + b -2;
+    printf("a = %d, c = %d, b = %d, d= %d\n", a,c,b,d);
+    c = c + 1, d = c - a + d;
+    printf("a = %d, c = %d, b = %d, d = %d\n", a,c,b,d);
+    a = a * c, c = c - 1;
+    printf("a = %d, c = %d, b = %d, d = %d\n", a,c,b,d);
+
+    a = a / 10, c = c / 2, b = b - 1, d = d * ( c + b + a);
+
+    printf("a = %d, c = %d, b = %d, d = %d\n", a,c,b,d);
     return 0;
 }
