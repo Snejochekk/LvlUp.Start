9949360fadbb8df6f679f1ec9f9c1b3c2fdcd17e
Snejochekk <Snejochekk@mail.ru>
2023-10-25 16:37:52 +0300

    Изменила имена переменных Исправила тип переменнной Поменяла решение с оператором "?:"

--- HWork_2-2/main.c
+++ HWork_2-2/main.c
@@ -5,28 +5,30 @@
 
 int main()
 {
-    int R = 6;
-    int a = 4;
-    int d = 7;
+    int radius_circle = 6;
+    int side_triangle = 4;
+    int side_square = 7;
+    printf("Dano:\n"
+           "________________________________\n"
+           "|radius_circle = %d\n"
+           "|side_triangle = %d\n"
+           "|side_square = %d\n", radius_circle, side_triangle, side_square);
+    printf("________________________________\n");
+    printf("opredelit' Naibol'shuyu S\n");
+    printf("________________________________\n");
     int SR;
     double Sa;
     int Sd;
     int Naibolshaya_ploshad;
-    printf("Dano:\n"
-           "________________________________\n"
-           "radius kruga        |R = %d\n"
-           "storona treugolnika |a = %d\n"
-           "storona kvadrata    |d = %d\n", R,a,d);
-    printf("________________________________\n");
-    printf("opredelit' Naibol'shuyu S\n");
-    printf("________________________________\n");
-    SR = M_PI * pow(R,2);
+    SR = M_PI * pow(radius_circle, 2);
     printf("SR = %d\n", SR);
-    Sa = sqrt(3) * pow(a,2)/4;
-    printf("Sa = %f\n",Sa);
-    Sd = pow(d,2);
+    Sa = sqrt(3) * pow(side_triangle, 2)/4;
+    printf("Sa = %f\n", Sa);
+    Sd = pow(side_square, 2);
     printf("Sd = %d\n", Sd);
-    Naibolshaya_ploshad = SR>Sa?SR:Sd;
+    Naibolshaya_ploshad = SR > Sa && SR > Sd ? SR :
+                                               Sa > Sd && Sa > SR ? Sa :
+                                                                    Sd > SR && Sd > Sa ? Sd:
     printf("________________________________\n");
     printf("Naibol'shaya S = %d\n", Naibolshaya_ploshad);
     printf("________________________________\n");
