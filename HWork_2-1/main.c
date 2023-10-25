4d1115d6c5f74e1e0d77d3587a818e6efbf8ec77
Snejochekk <Snejochekk@mail.ru>
2023-10-25 11:10:04 +0300

    Исправила "f" на "lf" Проставила запятые исправила "log2" на "log"

--- HWork_2-1/main.c
+++ HWork_2-1/main.c
@@ -6,14 +6,13 @@
 int main()
 {
     double x;
-    printf("vvedite x=");
-    scanf("%f",&x);
+    printf("vvedite x = ");
+    scanf("%lf", &x);
 
-    const double a=(cos(2)*(x-(M_PI/6))+sqrt(2));
-    const double c=1/log2(x)+pow(sin(pow(x,2)),2);
-    const double d=exp(3);
-    const double y=(a/c)*d;
-    printf("Result %f\n",y);
-
+    const double a = 2 * cos(x - (M_PI / 6)) + sqrt (2);
+    const double c = 1/log(x) + pow ( sin(pow(x, 2)), 2);
+    const double d = exp(3);
+    const double y = (a/c) * d;
+    printf("Result %f\n", y);
     return 0;
 }
