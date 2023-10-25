82a01fbe9540057f6ca5872139008399863cdd24
Snejochekk <Snejochekk@mail.ru>
2023-10-25 16:39:53 +0300

    .

--- HWork_2-2/main.c
+++ HWork_2-2/main.c
@@ -12,23 +12,25 @@ int main()
            "________________________________\n"
            "|radius_circle = %d\n"
            "|side_triangle = %d\n"
-           "|side_square = %d\n", radius_circle, side_triangle, side_square);
+           "|side_square = %d\n", radius_circle,
+           side_triangle, side_square);
     printf("________________________________\n");
     printf("opredelit' Naibol'shuyu S\n");
     printf("________________________________\n");
-    int SR;
-    double Sa;
-    int Sd;
+    double S_circle;
+    double S_triangle;
+    double S_square;
     int Naibolshaya_ploshad;
-    SR = M_PI * pow(radius_circle, 2);
-    printf("SR = %d\n", SR);
-    Sa = sqrt(3) * pow(side_triangle, 2)/4;
-    printf("Sa = %f\n", Sa);
-    Sd = pow(side_square, 2);
-    printf("Sd = %d\n", Sd);
-    Naibolshaya_ploshad = SR > Sa && SR > Sd ? SR :
-                                               Sa > Sd && Sa > SR ? Sa :
-                                                                    Sd > SR && Sd > Sa ? Sd:
+    S_circle = M_PI * pow(radius_circle, 2);
+    printf("S_circle = %lf\n", S_circle);
+    S_triangle = sqrt(3) * pow(side_triangle, 2)/4;
+    printf("S_triangle = %lf\n", S_triangle);
+    S_square = pow(side_square, 2);
+    printf("S_square = %lf\n", S_square);
+    Naibolshaya_ploshad = S_circle > S_triangle && S_circle > S_square ? S_circle :
+                                               S_triangle > S_square && S_triangle > S_circle ? S_triangle :
+                                                                    S_square > S_circle && S_square > S_triangle ? S_square:
+    printf("________________________________\n");
     printf("________________________________\n");
     printf("Naibol'shaya S = %d\n", Naibolshaya_ploshad);
     printf("________________________________\n");
